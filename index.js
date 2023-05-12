48d18c2e7057b7690e70b9b62d84364994da6f64
pavithran1999 <pavithranr65@gmail.com>
2023-05-12 23:59:06 +0530

    Added backend questions route

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.status(200).send("Express server created!!");
+let users = [];
+let questions = [
+  {
+    question: "This is a sample question",
+    answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+    correctAnswer: "answer 2",
+  },
+];
+
+//create a login function
+
+//create a signup function
+app.post("/signup", (req, res) => {
+  console.log(req, "req");
+  console.log(res, "res");
+});
+
+//create a questions route which will return all questions array
+app.get("/questions", (req, res) => {
+  res.status(200).send(questions);
 });
 
 app.listen(port, () => {
