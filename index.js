2cd3a7d73faf8eaee132c757fd471a691afe2d17
pavithran1999 <pavithranr65@gmail.com>
2023-05-13 00:46:56 +0530

    completed basic login, signup & other functions

--- index.js
+++ index.js
@@ -12,17 +12,39 @@ let questions = [
   },
 ];
 
+//middleware to handle req body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //create a login function
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  const logincheck = users.some(
+    (user) => user?.email === email && user.password === password
+  );
+  if (logincheck) {
+    res.status(200).send("Login succesful");
+  } else {
+    res.status(401).send("Invalid username or password");
+  }
+});
 
 //create a signup function
 app.post("/signup", (req, res) => {
-  console.log(req, "req");
-  console.log(res, "res");
+  const { email, password } = req.body;
+  users.push({ email, password });
+  res.status(200).send(req.body);
 });
 
 //create a questions route which will return all questions array
 app.get("/questions", (req, res) => {
   res.status(200).send(questions);
+});
+
+//get all users data for testing purpose
+app.get("/users", (req, res) => {
+  res.status(200).send(users);
 });
 
 app.listen(port, () => {
