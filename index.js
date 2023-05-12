9bec4c821fbd4297a9cae18a7692e9deebd68407
pavithran1999 <pavithranr65@gmail.com>
2023-05-13 01:21:58 +0530

    Updated more secure login & signup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 
 const port = 3000;
 
@@ -12,29 +13,72 @@ let questions = [
   },
 ];
 
+//functions
+function findUserByUsername(email) {
+  let user = users.find((eachuser) => eachuser?.email == email);
+  return user;
+}
+
+function createUser(email, password) {
+  const user = new User(email, password);
+  users.push(user);
+  return user;
+}
+
+//class to create user object
+class User {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+}
+
 //middleware to handle req body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //create a login function
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
-  const logincheck = users.some(
-    (user) => user?.email === email && user.password === password
-  );
-  if (logincheck) {
-    res.status(200).send("Login succesful");
-  } else {
-    res.status(401).send("Invalid username or password");
+  const user = findUserByUsername(email);
+
+  if (!user) {
+    return res.status(401).send("Invalid username or password");
+  }
+
+  try {
+    const isPasswordValid = await bcrypt.compare(password, user?.password);
+    if (isPasswordValid) {
+      res.status(200).send("Login successful!!");
+    } else {
+      res.status(401).send("Invalid username/password");
+    }
+  } catch (error) {
+    res.status(500).send("Internal server error");
   }
 });
 
 //create a signup function
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   const { email, password } = req.body;
-  users.push({ email, password });
-  res.status(200).send(req.body);
+
+  if (findUserByUsername(email)) {
+    return res.status(409).send("User already exist!!");
+  }
+
+  try {
+    //password hashing
+    //higher the salt more secure hash will be
+    const salt = 8;
+    const passwordHash = await bcrypt.hash(password, salt);
+    const user = createUser(email, passwordHash);
+
+    res.status(200).send("User created succesful");
+  } catch (error) {
+    res.status(500).send("Internal server error!!");
+  }
+
 });
 
 //create a questions route which will return all questions array
